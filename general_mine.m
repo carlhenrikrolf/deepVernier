1dc210a38fb2a0bdd575d76ffe9292f7fd7591bd
CarlHenrikRolf <henrikaslund@tsf-428-wpa-2-232.epfl.ch>
2017-05-03 14:13:42 +0200

    test matconvent training

---
+++ general_mine.m
@@ -0,0 +1,81 @@
+function out = general_mine
+
+setup;
+
+imdb = getImdb;
+
+% Get net
+f=1/100 ;
+net.layers = {} ;
+net.layers{end+1} = struct('type', 'conv', ...
+    'weights', {{f*randn(2,2,1,4, 'single'), []}}, ...
+    'stride', 1, ...
+    'pad', 0) ;
+net.layers{end+1} = struct('type', 'pool', ...
+    'method', 'max', ...
+    'pool', [2 2], ...
+    'stride', 2, ...
+    'pad', 0) ;
+net.layers{end+1} = struct('type', 'softmaxloss') ;
+
+net.meta.trainOpts.learningRate = 0.001 ;
+net.meta.trainOpts.numEpochs = 20 ;
+net.meta.trainOpts.batchSize = 1 ;
+
+% Fill in defaul values
+net = vl_simplenn_tidy(net) ;
+
+% run(fullfile(fileparts(mfilename('fullpath')),...
+%     '..', '..', 'matlab', 'vl_setupnn.m')) ;
+
+% Train
+trainfn = @cnn_train;
+opts.train = struct('gpus',[],...
+    'backPropDepth', 1) ;
+[net, info] = trainfn(net, imdb, getBatch, ...
+    net.meta.trainOpts, ...
+    opts.train, ...
+    'val', find(imdb.images.set == 3)) ;
+
+out = struct('net', net,...
+    'info',info,...
+    'imdb',imdb);
+
+    function imdb = getImdb
+        data = zeros(3,3,1,3,'single');
+        labels = zeros(1,3);
+        set_(1,3) = 1;
+        data(:,:,1,1) = [1 0 0
+                         0 1 0
+                         0 0 0];
+        labels(1) = 0;
+        set_(1) = 1;
+        data(:,:,1,2) = [0 1 0
+                         1 0 0
+                         0 0 0];
+        labels(2) = 1;
+        set_(2) = 1;
+        data(:,:,1,3) = [0 1 0
+                         1 0 0
+                         0 0 0];
+        labels(3) = 1;
+        set_(3) = 3;
+        
+        data_mean = mean(data,4);
+        
+        images = struct('data',data,...
+            'data_mean',data_mean,...
+            'labels',labels,...
+            'set',set_);
+        imdb = struct('images',images);
+    end
+
+    function fn = getBatch
+        fn = @(x,y) getSimpleNNBatch(x,y) ;
+    end
+
+    function [images, labels] = getSimpleNNBatch(imdb, batch)
+        images = imdb.images.data(:,:,:,batch) ;
+        labels = imdb.images.labels(1,batch) ;
+    end
+end
